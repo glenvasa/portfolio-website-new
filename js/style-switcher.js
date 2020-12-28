9e29704b4c2e8b49bb92dacde83f982d2da1fa21
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-12-28 14:32:25 -0500

    Added function themeMode() to set light/dark "theme" to localStorage and check localStorage whne page refreshes so theme persists

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -39,15 +39,34 @@ if (localStorage.getItem("color") !== null) {
 const dayNight = document.querySelector(".day-night");
 
 dayNight.addEventListener("click", () => {
-  dayNight.querySelector("i").classList.toggle("fa-sun");
-  dayNight.querySelector("i").classList.toggle("fa-moon");
   document.body.classList.toggle("dark");
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+  updateIcon();
 });
 
-window.addEventListener("load", () => {
+function themeMode() {
+  // checking if 'theme' key exists in localStorage
+  if (localStorage.getItem("theme") !== null) {
+    if (localStorage.getItem("theme") === "light") {
+      document.body.classList.remove("dark");
+    } else {
+      document.body.classList.add("dark");
+    }
+  }
+  updateIcon();
+}
+themeMode();
+
+function updateIcon() {
   if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.remove("fa-moon");
     dayNight.querySelector("i").classList.add("fa-sun");
   } else {
+    dayNight.querySelector("i").classList.remove("fa-sun");
     dayNight.querySelector("i").classList.add("fa-moon");
   }
-});
+}
